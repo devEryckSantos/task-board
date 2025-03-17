27da374a8ee7df663923db091c66fed0d1866195
Eryck Santos <dev.erycksantos@gmail.com>
2025-03-17 14:42:34 -0300

    Criação da camada de acesso aos dados

--- src/main/java/me/dio/service/BoardService.java
+++ src/main/java/me/dio/service/BoardService.java
@@ -1,7 +1,9 @@
 package me.dio.service;
 
 import lombok.AllArgsConstructor;
+import me.dio.persistence.dao.BoardColumnDAO;
 import me.dio.persistence.dao.BoardDAO;
+import me.dio.persistence.entity.BoardEntity;
 import org.springframework.stereotype.Service;
 
 import java.sql.Connection;
@@ -12,6 +14,26 @@ import java.sql.SQLException;
 public class BoardService {
 
     private final Connection connection;
+
+    public BoardEntity insert(final BoardEntity entity) throws SQLException {
+        var dao = new BoardDAO(connection);
+        var boardColumnDAO = new BoardColumnDAO(connection);
+        try {
+            dao.insert(entity);
+            var columns = entity.getBoardColumns().stream().map(c -> {
+               c.setBoard(entity);
+               return c;
+            }).toList();
+            for (var column : columns) {
+                boardColumnDAO.insert(column);
+            }
+            connection.commit();
+        } catch (SQLException e) {
+            connection.rollback();
+            throw e;
+        }
+        return entity;
+    }
 
     public boolean delete(final Long id) throws SQLException {
         var dao = new BoardDAO(connection);
